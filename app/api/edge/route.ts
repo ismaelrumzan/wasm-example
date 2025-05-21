025eaf5b9d953bc18ce75ba4c0b5dc89bb1b9e4a
ismaelrumzan <ismael@vercel.com>
2025-05-21 14:17:29 -0600

    initial

--- app/api/edge/route.ts
+++ app/api/edge/route.ts
@@ -4,11 +4,14 @@ import addWasm from "../../../add.wasm?module";
 
 const module$ = WebAssembly.instantiate(addWasm);
 
-export async function GET() {
+export async function GET(request: Request) {
+  const url = new URL(request.url)
+  const query = url.searchParams.get('query')
+  const num = Number(url.searchParams.get('number') || 10)
   const instance = (await module$) as any;
   const exports = instance.exports as typeof addWasmModule;
   const { add_one: addOne } = exports;
-  const number = addOne(10);
+  const number = addOne(num);
 
   return new Response(`got: ${number}`);
 }
